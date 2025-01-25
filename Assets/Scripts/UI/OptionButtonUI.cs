bddf33af901972491758880c9a6c1d22614673a9
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-25 19:38:39 +0100

    Add changes to main menu

---
+++ Assets/Scripts/UI/OptionButtonUI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class OptionButtonUI : MonoBehaviour
+{
+	private Button button;
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		button = GetComponent<Button>();
+		audioSource = FindAnyObjectByType<AudioSource>();
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			button.onClick.AddListener(OnClick);
+		}
+		else
+		{
+			button.onClick.RemoveListener(OnClick);
+		}
+	}
+
+	private void OnClick()
+	{
+		if(audioSource != null)
+		{
+			audioSource.Play();
+		}
+	}
+}

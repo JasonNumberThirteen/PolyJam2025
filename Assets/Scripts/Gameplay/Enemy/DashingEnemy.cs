883850dc2c571f5749be48d8eba8b14bd0b30c52
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-26 03:06:48 +0100

    Add graphics and animations for the dashing enemy

--- Assets/Scripts/Gameplay/Enemy/DashingEnemy.cs
+++ Assets/Scripts/Gameplay/Enemy/DashingEnemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class DashingEnemy : Enemy
 {
 	[SerializeField, Min(0f)] private float distanceToDash = 7f;
@@ -10,6 +11,8 @@ public class DashingEnemy : Enemy
 	private bool isDashing;
 	private bool isStopped;
 	private Vector2 dashPoint;
+	private Animator animator;
+	private SpriteRenderer spriteRenderer;
 
 	private readonly float DISTANCE_TO_DASH_POINT_THRESHOLD = 2f;
 	
@@ -24,6 +27,8 @@ public class DashingEnemy : Enemy
 		{
 			isDashing = true;
 			dashPoint = target.position + (target.transform.position - transform.position).normalized*additionalDistanceToDashPoint;
+
+			animator.SetBool("IsDashing", isDashing);
 		}
 
 		if(!isStopped && isDashing && IsCloseToPosition(dashPoint, DISTANCE_TO_DASH_POINT_THRESHOLD))
@@ -35,13 +40,28 @@ public class DashingEnemy : Enemy
 
 		var direction = (GetCurrentPosition() - (Vector2)transform.position).normalized;
 
+		if(spriteRenderer != null)
+		{
+			spriteRenderer.flipX = direction.x > 0;
+		}
+		
 		rb2D.velocity = direction*GetMovementSpeed();
+	}
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 	}
 
 	private void StopDashing()
 	{
 		isStopped = false;
 		isDashing = false;
+
+		animator.SetBool("IsDashing", isDashing);
 	}
 
 	private float GetMovementSpeed()
